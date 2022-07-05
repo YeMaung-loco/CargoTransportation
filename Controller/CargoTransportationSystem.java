d75c093527501e3eab3bffeb562022c057c44539
YeMaung-loco <yemaung969@gmail.com>
2022-07-05 22:54:29 +0630

    Staff Panel
    by LOCO 05/07/22

---
+++ Controller/CargoTransportationSystem.java
@@ -0,0 +1,9 @@
+package Controller;
+
+public class CargoTransportationSystem {
+
+	public static void main(String[] args) {
+		CreateAccount_controller createAccountController=new CreateAccount_controller();
+	}
+
+}
