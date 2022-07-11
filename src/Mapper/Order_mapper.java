652752da9d2d59604c75110f16e3812aa7457f45
YeMaung-loco <yemaung969@gmail.com>
2022-07-11 21:24:34 +0630

    order.java and ordermapper.java
    model and mapper for order

---
+++ src/Mapper/Order_mapper.java
@@ -0,0 +1,29 @@
+package Mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Model.Destination;
+import Model.Order;
+import Model.Weight;
+
+public class Order_mapper {
+	 public static Order mapper(Order order, ResultSet rs) {
+
+	        try {
+	            order.setOrder_id(rs.getInt("order_id"));
+	          Destination destination=new Destination();
+	            destination.setId(rs.getInt("destinationPrice_id"));
+	            destination.setDestinationname(rs.getString("destination_name"));
+	            Weight weight=new Weight();
+	            weight.setId(rs.getInt("weightPrice_id"));
+	            weight.setWeight_kg(rs.getInt("weight_kg"));
+	            weight.setWeightprice(rs.getInt("weight_price"));
+	            
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return order;
+	    }
+}
+
