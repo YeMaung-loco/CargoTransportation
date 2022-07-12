2b057f4fe43894b607c0fc81bb25db73d5e61d99
YeMaung-loco <yemaung969@gmail.com>
2022-07-12 13:14:04 +0630

    order and order mapper by Ye

--- src/Mapper/Order_mapper.java
+++ src/Mapper/Order_mapper.java
@@ -2,20 +2,16 @@ package Mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import Model.Customer;
 import Model.Destination;
 import Model.Order;
-import Model.Weight;
-import Model.Package;
 
 public class Order_mapper {
 	 public static Order mapper(Order order, ResultSet rs) {
-
 	        try {
 	            order.setOrder_id(rs.getInt("order_id"));
+	            
 	            Customer customer=new Customer();
 	            customer.setId(rs.getInt("customer_id"));
 	            customer.setName(rs.getString("c_name"));
@@ -27,11 +23,9 @@ public class Order_mapper {
 	            destination.setId(rs.getInt("destinationPrice_id"));
 	            destination.setDestinationname(rs.getString("destination_name"));
 	            order.setDestination(destination);
-	            
-	            
-	           List< Package> Package = new ArrayList<>();
-	            
+	               
 	            order.setTransportationfees(rs.getInt("transportation_fees"));
+	            order.setDate(rs.getDate("created_date"));
 	            
 	        } catch (SQLException e) {
 	            e.printStackTrace();
@@ -39,4 +33,3 @@ public class Order_mapper {
 	        return order;
 	    }
 }
-
--- src/Model/Order.java
+++ src/Model/Order.java
@@ -1,63 +1,64 @@
 package Model;
 
-import java.util.List;
+import java.sql.Date;
 
 public class Order {
-private int order_id;
-private  Destination  destination;
-private Weight weight;
-private Customer customer;
-private int transportationfees;
-private List<Package> packageList;
-public Order() {
-	
-}
-public List<Package> getPackageList() {
-	return packageList;
-}
-public void setPackageList(List<Package> packageList) {
-	this.packageList = packageList;
-}
-public Order(int order_id, Destination destination, Weight weight,Customer customer,Package packageList,int transfees) {
-	
-	this.order_id = order_id;
-	this.destination = destination;
-	this.weight = weight;
-	this.customer=customer;
-	this.transportationfees=transfees;
-}
-public int getTransportationfees() {
-	return transportationfees;
-}
-public void setTransportationfees(int transportationfees) {
-	this.transportationfees = transportationfees;
-}
-public int getOrder_id() {
-	return order_id;
-}
-public void setOrder_id(int order_id) {
-	this.order_id = order_id;
-}
-public Customer getcustomer(){
-	return customer;
-}
-public void setCustomer(Customer customer) {
-	this.customer=customer;
-}
-public Destination getDestination() {
-	return destination;
-}
-public void setDestination(Destination dest) {
-	this.destination = dest;
-}
-public Weight getWeight() {
-	return weight;
-}
-public void setWeight(Weight w) {
-	this.weight = w;
-}
+	private int order_id;
+	private Destination destination;
+	private Customer customer;
+	private int transportationfees;
+	Date date;
 
+	public Order() {
 
+	}
 
+	public Order(int order_id, Destination destination, Customer customer, int transfees) {
+
+		this.order_id = order_id;
+		this.destination = destination;
+		this.customer = customer;
+		this.transportationfees = transfees;
+	}
+
+	public int getTransportationfees() {
+		return transportationfees;
+	}
+
+	public void setTransportationfees(int transportationfees) {
+		this.transportationfees = transportationfees;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Destination getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Destination dest) {
+		this.destination = dest;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
 
 }
